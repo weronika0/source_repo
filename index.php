6919ffdbc9e3139feafd2320c3e12a9452c168e9
Weronika <weronika.nowak@pollub.edu.pl>
2024-01-14 18:53:49 +0100

    Add workflows and php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <title>Chmurki</title>
+</head>
+<body>
+    <h1>Programowanie fullstack w chmurze obliczeniowej</h1>
+    <h2>Zadanie 2</h2>
+    <h3>Autor: Weronika Nowak</h3>
+    <h4>Wersja: 01</h4>
+</body>
+</html>
